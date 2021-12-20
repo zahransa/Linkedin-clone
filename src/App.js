f3765164d9d5040b749182267aaa1396ae192d74
SaeedZahran <saeedzahran@hotmail.com>
2021-12-20 23:10:22 +0100

    Creating header.js and .css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="app">
      <h1>Lets build Linkedin </h1>
+
+     {/* Header */}
+     <Header />
+
+     {/* App Body */}
+       {/* Sidebar */}
+       {/* Feed */}
+       {/* Widgets */}
+
+
+
     </div>
   );
 }
