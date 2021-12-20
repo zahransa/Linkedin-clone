0d5bd0099e42744956d8ac1c2a0f8e6c5ceecdcb
SaeedZahran <saeedzahran@hotmail.com>
2021-12-21 00:21:05 +0100

    create 2 header containers

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from './Header';
 function App() {
   return (
     <div className="app">
-     <h1>Lets build Linkedin </h1>
+      
 
      {/* Header */}
      <Header />
--- src/Header.js
+++ src/Header.js
@@ -2,16 +2,22 @@ import React from "react"
 import './Header.css'
 
 function Header() {
+  return (
+    <div className="header">
+      <h1>This is the header</h1>
 
-    return (
-        <div className="header">
-            <h1>This is the header</h1>
+      <div  className="header__left">
 
+      </div>
 
-        </div>
+      <div  className="header__right">
 
+      </div>
 
-    )
+    </div>
+  );
+
+    
 }
 
 export default Header
