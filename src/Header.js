3834b129aaebc02b37cbc90be3321351b63346c7
SaeedZahran <saeedzahran@hotmail.com>
2021-12-21 03:52:36 +0100

    adding the linkedin logo

--- src/Header.js
+++ src/Header.js
@@ -6,25 +6,21 @@ import SearchIcon from '@material-ui/icons/Search';
 function Header() {
   return (
     <div className="header">
-      <h1>This is the header</h1>
+      
 
       <div  className="header__left">
-          <img src= "" alt="" />
+          <img
+           src= "https://cdn-icons-png.flaticon.com/512/174/174857.png" 
+           alt=""
+          />
 
           <div  className="header__search">
             <SearchIcon />
-            
-
-            <input type="text"/>
-          </div>
-          
-
+          <input type="text"/>
+        </div>
       </div>
 
-      <div  className="header__right">
-
-      </div>
-
+      <div  className="header__right"> </div>
     </div>
   );
 
